6c3a2c74e178c6e645037309c1a7fff55d958935
adcdenis <adcdenis@gmail.com>
2020-04-16 18:01:26 -0300

    pequena falha, esqueci de chamar método, nao gerou exceção e tb nao funcionou sacanagem.

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -1,3 +1,4 @@
+
 const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
@@ -5,7 +6,7 @@ const express = require('express')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true}))
-server.use(bodyParser.json)
+server.use(bodyParser.json())
 
 server.listen(port, function() {
 
