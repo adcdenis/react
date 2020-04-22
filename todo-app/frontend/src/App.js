bd0462268380efc6eda66c449d6511eee0b9f73f
adcdenis <adcdenis@gmail.com>
2020-04-22 01:08:59 -0300

    primeiros componentes front end

--- todo-app/frontend/src/App.js
+++ todo-app/frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'reactstrap';
-import { Jumbotron } from 'reactstrap';
+import Todo from './todo/todo'
+//import { Button } from 'reactstrap';
+//import { Jumbotron } from 'reactstrap';
 
 export default props => (
-    <Jumbotron>
-      <Button color="danger">Danger!</Button>
-    </Jumbotron>
+ <Todo valor="inicio react"/>  
 )
--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,1 +1,15 @@
 import React from 'react'
+import About from '../template/about'
+import Header from '../template/header'
+
+export default class Todo extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <About/>
+                <Header/>
+            </div>
+        )
+    }
+}
