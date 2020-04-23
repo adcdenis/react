0412a415625b51df03a4a24f1f877159e430de4b
adcdenis <adcdenis@gmail.com>
2020-04-23 18:59:47 -0300

    ajuste tamanho

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default props => (
     <div role='form' className='todoForm'>
-        <div className='col-xs-12 col-sm-9 col-md-10'>
+        <div className='col-xs-12 col-sm-3 col-md-10'>
             <input className='form-control' placeholder='digite aqui uma tarefa'/>
         </div>
-        <div className='col-xs-12 col-sm-9 col-md-10'>
+        <div className='col-xs-12 col-sm-3 col-md-2'>
             <button className='btn btn-primary'>
                 <i className='fa fa-plus'/>
             </button>
