2ecd2f869585dd32c96fb6c5fb742cbcdaccdb4c
adcdenis <adcdenis@gmail.com>
2020-04-24 18:31:24 -0300

    componet grid

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Grid from '../template/grid'
 
 export default props => (
     <div role='form' className='todoForm'>
-        <div className='col-xs-12 col-sm-3 col-md-10'>
+       
+        <Grid xs='12' sm='3' md='10'>
             <input className='form-control' placeholder='digite aqui uma tarefa'/>
-        </div>
-        <div className='col-xs-12 col-sm-3 col-md-2'>
+        </Grid>    
+        <Grid xs='12' sm='3' md='2'>
             <button className='btn btn-primary'>
                 <i className='fa fa-plus'/>
             </button>
-        </div>
-
+        </Grid>
     </div>
 )
