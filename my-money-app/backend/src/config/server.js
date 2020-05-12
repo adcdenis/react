1971a410574d519bac314fc068357bca5dc0b981
adcdenis <adcdenis@gmail.com>
2020-05-12 18:35:46 -0300

    backend finalizado

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -1,11 +1,18 @@
+const cors = require('./cors')
+const queryParser = require('express-query-int')
+
 const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(queryParser())
+
+server.use(cors)
+
 server.listen(port, function() {
- console.log(`BACKEND is running on port ${port}.`)
+ console.log(`BACKEND is rodando na porta ${port}.`)
 })
 
 module.exports = server
