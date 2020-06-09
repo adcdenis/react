0e85167242759b310d955cb0c01ad12c057ea338
adcdenis <adcdenis@gmail.com>
2020-06-09 19:18:00 -0300

    outra tentativa

--- my-money-app/frontend/src/consts.js
+++ my-money-app/frontend/src/consts.js
@@ -1,7 +1,5 @@
 export default {
 
-  API_URL: ' https://adcdenis-my-money-app-backend.herokuapp.com/api',
-  OAPI_URL: 'https://adcdenis-my-money-app-backend.herokuapp.com/oapi'
-  //API_URL: 'http://localhost:3003/api',
-  //OAPI_URL: 'http://localhost:3003/oapi',
+  API_URL: process.env.AMBIENTE ? 'https://adcdenis-my-money-app-backend.herokuapp.com/api' : 'http://localhost:3003/api',
+  OAPI_URL: process.env.AMBIENTE ? 'https://adcdenis-my-money-app-backend.herokuapp.com/oapi' : 'http://localhost:3003/oapi'
 }
