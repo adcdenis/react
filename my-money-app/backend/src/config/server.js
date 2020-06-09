72ce28c2fd7122d9e76c9a75cda3a1156e2f8d62
adcdenis <adcdenis@gmail.com>
2020-06-09 02:15:19 -0300

    mudando porta dinamica do env

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -1,7 +1,7 @@
 const cors = require('./cors')
 const queryParser = require('express-query-int')
 
-const port = 80
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
