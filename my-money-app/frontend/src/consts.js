839e56d7e2c6ee1a9261eefda369851ca6339341
adcdenis <adcdenis@gmail.com>
2020-06-12 16:27:26 -0300

    logs pra tentar idenficar algo no mobile

--- my-money-app/frontend/src/consts.js
+++ my-money-app/frontend/src/consts.js
@@ -1,8 +1,8 @@
 export default {
 
-  //API_URL: 'https://adcdenis-my-money-app-backend.herokuapp.com/api',
-  //OAPI_URL: 'https://adcdenis-my-money-app-backend.herokuapp.com/oapi'
+  API_URL: 'https://adcdenis-my-money-app-backend.herokuapp.com/api',
+  OAPI_URL: 'https://adcdenis-my-money-app-backend.herokuapp.com/oapi'
 
-  API_URL: process.env.AMBIENTE ? 'https://adcdenis-my-money-app-backend.herokuapp.com/api' : 'http://localhost:3003/api',
-  OAPI_URL: process.env.AMBIENTE ? 'https://adcdenis-my-money-app-backend.herokuapp.com/oapi' : 'http://localhost:3003/oapi'
+  //API_URL: process.env.AMBIENTE ? 'https://adcdenis-my-money-app-backend.herokuapp.com/api' : 'http://localhost:3003/api',
+  //OAPI_URL: process.env.AMBIENTE ? 'https://adcdenis-my-money-app-backend.herokuapp.com/oapi' : 'http://localhost:3003/oapi'
 }
